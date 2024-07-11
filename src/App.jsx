7401527a6cf159bb2408cd78a7bbf3ae43095e7c
Angelo <114301519+AngeloDeveD@users.noreply.github.com>
2024-07-11 15:41:05 +0700

    Create Generator

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [password, setPassword] = useState('');
+  const [length, setLength] = useState(256);
+  const [isCopied, setIsCopied] = useState(false);
+
+  const generatePassword = (length) => {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      result += characters[randomIndex];
+    }
+    return result;
+  };
+
+  const handleGenerate = () => {
+    const newPassword = generatePassword(length);
+    setPassword(newPassword);
+    setIsCopied(false);
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(password);
+    setIsCopied(true);
+  };
+
+  return (
+    <div className="App">
+      <h1>Генератор паролей</h1>
+      <label>
+        Длина пароля:
+        <input
+          type="number"
+          value={length}
+          onChange={(e) => setLength(e.target.value)}
+          min="1"
+          max="128"
+        />
+      </label>
+      <button onClick={handleGenerate}>Сгенерировать пароль</button>
+      {password && (
+        <div className='App__downer'>
+          <p>{password}</p>
+          <button onClick={handleCopy}>Копировать</button>
+          {isCopied && (<p>Пароль скопирован!</p>)}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
